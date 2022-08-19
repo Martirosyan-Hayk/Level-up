5137cfa9a30bf60f92aa812227552da93868445d
Martirosyan-Hayk <hayk_martirosyan_2020@mail.ru>
2022-08-19 22:26:48 +0400

    added binarySearchRecursive

--- src/binary-search/binary-search.ts
+++ src/binary-search/binary-search.ts
@@ -1,11 +1,8 @@
-function binarySearch(array: number[], key: number){
+function binarySearch(array: number[], start: number, end: number, key: number){
 	
 	if(array.length == 0){
 		return -1;
 	}
-
-	let start: number = 0;
-	let end: number = array.length - 1;
 	
 	while(start <= end) {
 		
@@ -23,6 +20,23 @@ function binarySearch(array: number[], key: number){
 	return -1;
 }
 
+function binarySearchRecursive(array: number[], start: number, end: number, key: number){
+	
+	if(start >= end){
+		return -1;
+	}
+
+	let middle: number = ~~((start + end) / 2);
+
+	if(array[middle] == key) {
+		return middle;
+	} else if (arr[middle] > key) {
+		return binarySearchRecursive(array, start, middle - 1, key);
+	} else {
+		return binarySearchRecursive(array, middle + 1, end, key);
+	}
+}
+
 let arr: number[] = [];
 for(var i = 0; i < 1; i++){
    for(var j = 0; j < Math.floor(Math.random() * 50); j++){
@@ -35,4 +49,5 @@ const sortedArr: number[] = arr.sort(function(a, b) {
   });
 
 
-console.log(binarySearch(sortedArr, 8));
+console.log(binarySearch(sortedArr, 0, sortedArr.length, 8));
+console.log(binarySearchRecursive(sortedArr, 0, sortedArr.length, 8));
