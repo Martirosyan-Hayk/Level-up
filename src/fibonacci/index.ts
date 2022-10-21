d2a77b600c68fca1a6ab0791c6263156577617e6
Martirosyan-Hayk <hayk_martirosyan_2020@mail.ru>
2022-10-21 14:45:36 +0400

    added interview questions

--- src/fibonacci/index.ts
+++ src/fibonacci/index.ts
@@ -1,5 +1,5 @@
 function fibonacci(n: number): number {
-    if(n < 2){
+    if (n < 2) {
         return n;
     }
 
@@ -7,17 +7,35 @@ function fibonacci(n: number): number {
 }
 
 function fibonacciWithCache(n: number): number {
-    if(n < 2){
+    if (n < 2) {
         return n;
     }
 
     fibonacciWithCache.prototype.cache = fibonacciWithCache.prototype.cache ?? [];
+
     if (!fibonacciWithCache.prototype.cache[n]) {
-        fibonacciWithCache.prototype.cache[n] = fibonacciWithCache(n - 1) + fibonacciWithCache(n - 2);
+        fibonacciWithCache.prototype.cache[n] =
+            fibonacciWithCache(n - 1) + fibonacciWithCache(n - 2);
     }
+
     return fibonacciWithCache.prototype.cache[n];
 }
 
+function fibonacciIterable(n: number): number {
+    if (n <= 0) {
+        return -1;
+    }
+
+    const result = [1, 1];
+
+    for (let i = 2; i < n; i++) {
+        result[i] = result[i - 2] + result[i - 1];
+    }
+
+    return result[result.length - 1];
+}
+
+console.log(fibonacciIterable(10));
 console.log(fibonacciWithCache(5));
 console.log(fibonacciWithCache(6));
 console.log(fibonacciWithCache(7));
